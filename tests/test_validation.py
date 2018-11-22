ac741925fe3d1751ac36b1a0a3b47602d566b221
ashwallsinfinity <ashley.walls@infinityworks.com>
2018-11-22 18:01:34 +0000

    add validation and tests

---
+++ tests/test_validation.py
@@ -0,0 +1,24 @@
+import unittest
+from validation.validation import Validation
+from validation.validate_fields import bamboo_fields
+
+
+class TestValidation(unittest.TestCase):
+
+    validator = Validation()
+
+    def test_valid_fields_returns_true(self):
+        field = 'firstName'
+        result = self.validator.valid_fields(field)
+        self.assertTrue(result)
+
+    
+    def test_valid_fields_returns_false(self):
+        field = 'primaryName'
+        result = self.validator.valid_fields(field)
+        self.assertFalse(result)
+
+    def test_fields_to_url_returns_string(self):
+        fields = ('firstName', 'lastName')
+        result = self.validator.fields_to_url(fields)
+        self.assertEquals(result, 'firstName,lastName')
