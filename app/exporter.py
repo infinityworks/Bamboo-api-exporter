147709b468c059c5823843678f4bda1130b6d3aa
ashwallsinfinity <ashley.walls@infinityworks.com>
2018-11-23 13:52:57 +0000

    refactor base url and headers

--- app/exporter.py
+++ app/exporter.py
@@ -3,86 +3,70 @@ import requests
 import json
 
 
-class BambooHrApi():
-
-    validation = Validation()
+class bambooHrApi():
 
 
     def __init__(self, api_key, organisation):
+
+        # Organisation API key
         self.api_key = str(api_key)
+
+        # Organisation name
         self.organisation = str(organisation)
+
+        # BambooHR base url used in api requests
+        self.base_url = "https://api.bamboohr.com/api/gateway.php/"
+
+        # Headers setting API key, JSON datatype and cache control
+        self.headers = {
+                        'Authorization': 'Basic ' + self.api_key,
+                        'Accept': 'application/json',
+                        'Cache-Control': 'no-cache'
+                        }
+
+        # Validation class used to validate arguments and api response
+        self.validation = Validation()
 
     
     def get_table(self, employee_id, table_name):
-        """Returns a json structure containing all rows for a specified employee ID and table combination"""
+        """Returns a json datatype containing all rows for a specified employee ID and table combination"""
 
-        url = "https://api.bamboohr.com/api/gateway.php/" + self.organisation + "/v1/employees/" + str(
-            employee_id) + "/tables/" + str(table_name)
+        url = self.base_url + self.organisation + "/v1/employees/" + str(employee_id) + "/tables/" + str(table_name)
 
-        headers = {
-            "headers": {
-                'Authorization': 'Basic ' + self.api_key,
-                'Accept': 'application/json',
-                'Cache-Control': 'no-cache'
-            }
-        }
-
-        response = requests.get(url, headers=headers['headers'], timeout=10)
-        if response.status_code == requests.codes.ok:
+        try:
+            response = requests.get(url, headers=self.headers, timeout=10)
             response = json.loads(response.text)
-
             return response
-        else:
-            response.raise_for_status()
+        except Exception as error:
+            return error
 
 
     def get_employee(self, employee_id, *args):
-        """Returns a json structure for a specified employee ID and any field names entered """
+        """Returns a json datatype for a specified employee ID and any field names entered """
 
-        #check args entered are valid fields in bamboo
+        #check args match bamboo valid fields list
         self.validation.valid_fields(args)
 
-        url = "https://api.bamboohr.com/api/gateway.php/" + self.organisation + "/v1/employees/" + str(
-            employee_id) + "?fields=" + self.validation.fields_to_url(args)
+        url = self.base_url + self.organisation + "/v1/employees/" + str(employee_id) + "?fields=" + self.validation.fields_to_url(args)
 
-        headers = {
-            "headers": {
-                'Authorization': 'Basic ' + self.api_key,
-                'Accept': 'application/json',
-                'Cache-Control': 'no-cache'
-            }
-        }
-
-        response = requests.get(url, headers=headers['headers'], timeout=10)
-        if response.status_code == requests.codes.ok:
+        try:
+            response = requests.get(url, headers=self.headers, timeout=10)
             response = json.loads(response.text)
-
             return response
-        else:
-            response.raise_for_status()
+        except Exception as error:
+            return error
+
 
 
     def custom_report(self, report_id):
-        """Returns a json structure using BambooHR customer report ID"""
+        """Returns a json datatype using BambooHR customer report ID"""
 
-        url = "https://api.bamboohr.com/api/gateway.php/" + self.organisation + "/v1/reports/" + str(
-            report_id) + "?format=JSON&fd=yes"
+        url = self.base_url + self.organisation + "/v1/reports/" + str(report_id) + "?format=JSON&fd=yes"
 
-        headers = {
-            "headers": {
-                'Authorization': 'Basic ' + self.api_key,
-                'Accept': 'application/json',
-                'Cache-Control': 'no-cache'
-            }
-        }
-
-        response = requests.get(url, headers=headers['headers'], timeout=10)
+        response = requests.get(url, headers=self.headers, timeout=10)
         if response.status_code == requests.codes.ok:
             response = json.loads(response.text)
 
             return response
         else:
             response.raise_for_status()
-
-
-#if __name__ == '__main__':
