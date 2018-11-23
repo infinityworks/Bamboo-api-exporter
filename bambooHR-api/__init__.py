f53dadfff16706aedd0212ddd3850c499187ef91
ashwallsinfinity <ashley.walls@infinityworks.com>
2018-11-23 16:23:38 +0000

    change package name

--- bambooHR-api/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.bambooHR-api import bambooHrApi
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bambooHR_api',
+      version='0.1',
+      description='Python wrapper for BambooHR api',
+      url='https://github.com/infinityworks/Bamboo-api-exporter',
+      author='Ashley Walls and James Sheard',
+      license='MIT',
+      packages=['bambooHR_api'],
+      zip_safe=False)
